618e346d324cd7c7cbc5ddb81faa3dd6bccf364b
Richard Osborne <richard@xmos.com>
2011-04-12 18:14:25 +0100

    Add otp read function (not yet working).

--- module_jtag_otp_access/src/jtag_otp_access.c
+++ module_jtag_otp_access/src/jtag_otp_access.c
@@ -2,7 +2,61 @@
 #include "jtag.h"
 #include <xclib.h>
 
-/* Test port commands. */
+/* Test port mode register. */
+
+#define MODE_SELECT_SHIFT 0
+#define MODE_SELECT_SIZE 1
+#define MODE_SELECT_MASK (((1 << MODE_SELECT_SIZE) - 1) << MODE_SELECT_SHIFT)
+
+#define MODE_SELECT_ENABLE_VAL 1
+#define MODE_SELECT_DISABLE_VAL 0
+
+#define MODE_SELECT_ENABLE (MODE_SELECT_ENABLE_VAL << MODE_SELECT_SHIFT)
+#define MODE_SELECT_DISABLE (MODE_SELECT_DISABLE_VAL << MODE_SELECT_SHIFT)
+
+#define TSO_SELECTOR_SHIFT 1
+#define TSO_SELECTOR_SIZE 2
+#define TSO_SELECTOR_MASK (((1 << TSO_SELECTOR_SIZE) - 1) << TSO_SELECTOR_SHIFT)
+
+#define TSO_SELECTOR_STATUS_VAL 0
+#define TSO_SELECTOR_SO_VAL 1
+#define TSO_SELECTOR_SR_VAL 2
+
+#define TSO_SELECTOR_STATUS (TSO_SELECTOR_STATUS_VAL << TSO_SELECTOR_SHIFT)
+#define TSO_SELECTOR_SO (TSO_SELECTOR_SO_VAL << TSO_SELECTOR_SHIFT)
+#define TSO_SELECTOR_SR (TSO_SELECTOR_SR_VAL << TSO_SELECTOR_SHIFT)
+
+#define BURST_MODE_SHIFT 3
+#define BURST_MODE_SIZE 2
+#define BURST_MODE_MASK (((1 << BURST_MODE_SIZE) - 1) << BURST_MODE_SHIFT)
+
+#define BURST_MODE_NONE_VAL 0
+
+#define BURST_MODE_NONE (BURST_MODE_NONE_VAL << BURST_MODE_SHIFT)
+
+#define AUX_UPDATE_SHIFT 8
+#define AUX_UPDATE_SIZE 1
+#define AUX_UPDATE_MASK (((1 << AUX_UPDATE_SIZE) - 1) << AUX_UPDATE_SHIFT)
+
+#define AUX_UPDATE_ENABLE_VAL 1
+#define AUX_UPDATE_DISABLE_VAL 0
+
+#define AUX_UPDATE_ENABLE (AUX_UPDATE_ENABLE_VAL << AUX_UPDATE_SHIFT)
+#define AUX_UPDATE_DISABLE (AUX_UPDATE_DISABLE_VAL << AUX_UPDATE_SHIFT)
+
+#define MACRO_SELECT_SHIFT 9
+#define MACRO_SELECT_SIZE 1
+#define MACRO_SELECT_MASK (((1 << MACRO_SELECT_SIZE) - 1) << MACRO_SELECT_SHIFT)
+
+#define MACRO_SELECT_ENABLE_VAL 1
+#define MACRO_SELECT_DISABLE_VAL 0
+
+#define MACRO_SELECT_ENABLE (MACRO_SELECT_ENABLE_VAL << MACRO_SELECT_SHIFT)
+#define MACRO_SELECT_DISABLE (MACRO_SELECT_DISABLE_VAL << MACRO_SELECT_SHIFT)
+
+#define MODE_REGISTER_LEN 6
+
+/* Test port command register. */
 enum {
   TP_IDLE = 0x0,
   TP_DIRECT = 0x1,
@@ -13,6 +67,24 @@ enum {
   TP_UPDATE_CMD = 0x6,
   TP_INC_ADDR = 0x7
 };
+#define CMD_REGISTER_LEN 6
+
+/* Macro command register. */
+#define COMP_SHIFT 0
+#define PCH_SHIFT 1
+#define PGM_SHIFT 2
+#define READ_SHIFT 3
+#define WRITE_SHIFT 4
+#define RESET_SHIFT 5
+
+#define COMP (1 << COMP_SHIFT)
+#define PCH (1 << PCH_SHIFT)
+#define PGM (1 << PGM_SHIFT)
+#define READ (1 << READ_SHIFT)
+#define WRITE (1 << WRITE_SHIFT)
+#define RESET (1 << RESET_SHIFT)
+
+#define DONTCARE 0
 
 /* Test port commands. */
 static inline void update_command(int chipmodule)
@@ -41,4 +113,29 @@ static unsigned shift(int chipmodule, unsigned data)
   jtag_module_otp_write_test_port_cmd(chipmodule, TP_SHIFT);
   data = jtag_module_otp_shift_data(chipmodule, bitrev(data));
   return bitrev(data);
+}
+
+/* Helper functions. */
+static void write_mode(int chipmodule, unsigned value, unsigned address)
+{
+  unsigned data = value | (address << MODE_REGISTER_LEN);
+  shift(chipmodule, data);
+  update_mode(chipmodule);
+}
+
+static void write_command(int chipmodule, unsigned command, unsigned address)
+{
+  unsigned data = command | (address << CMD_REGISTER_LEN);
+  shift(chipmodule, data);
+  update_command(chipmodule);
+}
+
+/* OTP functions */
+unsigned jtag_otp_read_word(int chipmodule, unsigned address)
+{
+  write_mode(chipmodule, MODE_SELECT_DISABLE | TSO_SELECTOR_SO |
+                         BURST_MODE_NONE | MACRO_SELECT_ENABLE, address);
+  write_command(chipmodule, READ, address);
+  idle(chipmodule);
+  return direct_access(chipmodule, DONTCARE);
 }
--- module_jtag_otp_access/src/jtag_otp_access.h
+++ module_jtag_otp_access/src/jtag_otp_access.h
@@ -1,6 +1,6 @@
 #ifndef JTAG_OTP_ACCESS_H_
 #define JTAG_OTP_ACCESS_H_
 
+unsigned jtag_otp_read_word(int chipmodule, unsigned address);
 
-
-#endif JTAG_OTP_ACCESS_H_
+#endif /* JTAG_OTP_ACCESS_H_ */
